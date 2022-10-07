c2ba3c15317db1210d8ac0a04851e7c4e9d63d74
Md Hasibur Rahman <mdhasiburrahman34@gmail.com>
2022-10-07 17:36:39 +0600

    first commit

---
+++ assignment1.py
@@ -0,0 +1,78 @@
+a=20
+b=10
+
+#Add
+ans=a+b
+
+print(ans)
+#Sum 30
+
+
+#Sub
+ans=a-b
+
+print(ans)
+#10
+
+#multiplication
+ans=a*b
+
+print(ans)
+
+#200
+
+#division
+ans=a/b
+
+print(ans)
+
+
+
+#Exponentiation
+ans=a**b
+
+print(ans)
+#Exponentiation
+
+
+x=10
+y=20
+#==
+if x==y:
+    print("Value of x and y are equal.")
+else:
+    print("Condition Vul")
+#
+
+#!=
+if x!=y:
+     print("Value of x and y are not equal.")
+else:
+    print("Condition Vul")
+
+#>
+if x>y:
+     print("Value of x is greater than y.")
+else:
+    print("Condition Vul")
+
+
+#<
+if x<y:
+     print("Value of x is less than y")
+else:
+    print("<Condition Vul")
+
+#>=
+if x>=y:
+     print("Value of x is greater than or equal to y")
+else:
+    print("Condition Vul")
+
+#<=
+if x<=y:
+     print("Value of x is less than or equal to y")
+else:
+    print("<= Condition failed")
+
+
