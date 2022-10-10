e3148eb007e4af7de622501f1e9e3a2d9a854044
Md Hasibur Rahman <mdhasiburrahman34@gmail.com>
2022-10-10 18:34:05 +0600

    class3

---
+++ class3.py
@@ -0,0 +1,29 @@
+# a=5
+# b=3
+
+# print(a&b)
+
+# name='hasibur'
+# dept='cse'
+
+mu_list=['name','dept','name','dept','namenai','deptent']
+# print(mu_list[4])
+print(len(mu_list))
+
+# my_tuple=('hsibur','name','dept','name','dept','namenai','deptent')
+# print(my_tuple)
+
+# my_set={'hsibur','name','dept','name','dept','namenai','deptent'}
+# print(my_set)
+
+my_dic={'name' :'md hasibur','dept':'cse','roll':3280}
+print(my_dic)
+for a in my_dic:
+    print(a)
+# mu_list=['name','dept','name','dept','namenai','deptent']
+# for a in mu_list:
+#     print(a)
+
+# for a in range(13,131,13):
+#     print(a)
+    
